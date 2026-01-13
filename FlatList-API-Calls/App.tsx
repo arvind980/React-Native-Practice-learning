99aa6584fe49b498e0ba767ddea6d3d75c74d090
arvind980 <arvind.rajput0524@gmail.com>
2026-01-14 00:06:14 +0530

    show data in flat list after api call

--- FlatList-API-Calls/App.tsx
+++ FlatList-API-Calls/App.tsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import apiManager from './src/API/APIManager';
-
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 interface Todo {
   id: number;
   title: string;
 }
 
 export default function App() {
+
   const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
@@ -23,26 +24,38 @@ export default function App() {
 
     loadTodos();
   }, []);
+
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={todos}
-        keyExtractor={(item: Todo) => item.id.toString()}
-        renderItem={({ item }: { item: Todo }) => (
-          <Text>{item.title}</Text>
-        )}
-      />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container} >
+        <FlatList
+          data={todos}
+          keyExtractor={(item: Todo) => item.id.toString()}
+          renderItem={({ item }: { item: Todo }) => (
+            <Text style={styles.todoItem}>{item.title}</Text>
+          )}
+        />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    //marginVertical: 50,
+  },
+  todoItem: {
+    fontSize: 17,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    fontWeight: 'bold',
+    color: '#333',
   },
 });
+
+
--- FlatList-API-Calls/src/API/APIClient.tsx
+++ FlatList-API-Calls/src/API/APIClient.tsx
@@ -5,4 +5,21 @@ const apiClient = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com',
 });
 
+
+
+apiClient.interceptors.request.use(config => {
+    // You can modify the request config here if needed
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+apiClient.interceptors.response.use(response => {
+    if (response.status === 200) {
+        console.log('API Response:', response.data);
+    }
+    return response;
+}, error => {
+    return Promise.reject(error);
+});
+
 export default apiClient;
